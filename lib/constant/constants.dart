4f944d5a94dd91556e5a1a8959300ee76dd8bb8a
ayushii019 <ayushiverma192002@gmail.com>
2022-08-12 23:05:02 +0530

    commiting

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -5,9 +5,9 @@ String nftStorageApiKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
 
 const String rpcUrl =
-    "https://ropsten.infura.io/v3/e43345b7383246378963be7acd5b6c67";
-//"http://127.0.0.1:7545";
+    //"https://ropsten.infura.io/v3/e43345b7383246378963be7acd5b6c67";
+"http://127.0.0.1:7545";
 
-const String contractAddress = "0x702058Ba021Cd4e4F847B40F32b58aA5be3a4661";
+const String contractAddress = "0xed690C24C60A48F8A9819c9A15AD75B70CFBEa5a";
 
-const int chainId = 3; //Ropsten-3, Polygon testnet-80001, local ganache-1337
+const int chainId = 1337; //Ropsten-3, Polygon testnet-80001, local ganache-1337
